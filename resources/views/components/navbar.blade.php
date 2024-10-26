fd27370870dc8eb5311f756efc0b41d5ff9bf00b
ardianilyas <ardianilyas@gmail.com>
2024-10-26 21:39:17 +0700

    update navbar profile

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,9 +21,8 @@
         <div :class="open ? '' : 'hidden md:block' ">
             @auth
             <div class="hs-dropdown relative inline-flex">
-                <button id="hs-dropdown-default" type="button" class="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
-                    {{ auth()->user()->name }}
-                    <svg class="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+                <button id="hs-dropdown-default" type="button" class="hs-dropdown-toggle p-1 flex items-end justify-end overflow-hidden gap-x-2 text-sm font-medium rounded-full bg-gray-100 text-gray-800 shadow-sm hover:bg-gray-200 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                    <img src="https://api.dicebear.com/9.x/notionists/svg?seed=Alexander" class="w-12 rounded-full bottom-0" alt="avatar" />
                 </button>
                 
                 <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
