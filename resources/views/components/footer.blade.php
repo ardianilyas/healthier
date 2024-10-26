7f38e2431dbcce386a5dbb9178e4236a99d66869
ardianilyas <ardianilyas@gmail.com>
2024-10-26 19:58:32 +0700

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,30 @@
-<footer class="bg-gray-200 w-full absolute bottom-0 right-0">
-    <div class="container py-6">
-        copyright &copy; 2024
+<footer class="bg-[#0A0E31] text-white text-sm w-full absolute bottom-0 right-0">
+    <div class="w-full px-8 md:px-10 lg:px-12 py-16">
+        <div class="w-full grid grid-cols-8">
+            <div class="col-span-6">
+                <h1>Healthier</h1>
+                <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, veniam!</p>
+            </div>
+            <div class="">
+                <h3 class="text-lg font-semibold">Company Info</h3>
+                <ul class="mt-2 flex flex-col gap-2">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Artikel</a></li>
+                    <li><a href="">Pesan Obat</a></li>
+                    <li><a href="">Konsultasi</a></li>
+                    <li><a href="">Membership</a></li>
+                </ul>
+            </div>
+            <div class="">
+                <h3 class="text-lg font-semibold">Kontak Kami</h3>
+                <ul class="mt-2 flex flex-col gap-2">
+                    <li>+626564465455</li>
+                    <li>healthier@gmail.com</li>
+                    <li>Telp: +6246544654</li>
+                </ul>
+            </div>
+        </div>
+        <div class="w-full my-6 bg-gray-500 h-[0.5px]"></div>
+        <p class="text-center font-medium">Copyright &copy; 2024</p>
     </div>
 </footer>
