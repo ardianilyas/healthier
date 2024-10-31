9787ae62eee60103396ac321fd92a7defac8d88d
ardianilyas <ardianilyas@gmail.com>
2024-10-31 22:20:49 +0700

    add karyawan menu

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -70,6 +70,14 @@
                                     Obat
                                 </a>
                             </li>
+                            <li>
+                                <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-white rounded-lg hover:bg-white/10 focus:outline-none focus:bg-white/10 dark:bg-neutral-700 dark:text-white {{ request()->routeIs('dashboard.obat*') ? 'bg-white/10' : '' }}" href="/dashboard/obat">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                    </svg>                                                                            
+                                    Karyawan
+                                </a>
+                            </li>
                         </ul>
                     </nav>
                 </div>
