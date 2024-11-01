835a8cd731db191a6495da619cfe1c01a79905a4
ardianilyas <ardianilyas@gmail.com>
2024-11-01 19:47:11 +0700

    update dashboard menu bar

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -79,6 +79,14 @@
                                     Karyawan
                                 </a>
                             </li>
+                            <li>
+                                <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-white rounded-lg hover:bg-white/10 focus:outline-none focus:bg-white/10 dark:bg-neutral-700 dark:text-white" href="{{ route('homepage') }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                    </svg>                                                                                                             
+                                    Homepage
+                                </a>
+                            </li>
                         </ul>
                     </nav>
                 </div>
