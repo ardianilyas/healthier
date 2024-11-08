d19ef13f728aa4ba99176a6daeebb08d7c8dba27
ardianilyas <ardianilyas@gmail.com>
2024-11-08 22:14:36 +0700

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="bg-[#0A0E31] text-white text-sm w-full">
     <div class="w-full px-8 md:px-10 lg:px-12 pt-10 pb-4">
-        <div class="w-full grid grid-cols-8">
-            <div class="col-span-6">
-                <h1>Healthier</h1>
-                <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, veniam!</p>
+        <div class="w-full grid grid-rows-2 md:grid-rows-none lg:grid-cols-8">
+            <div class="lg:col-span-6">
+                <h1 class="text-2xl font-semibold">Healthier</h1>
+                <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsa incidunt doloribus quo sit, neque laboriosam a commodi maxime facere saepe at obcaecati ducimus aut sapiente nulla fugiat iste cupiditate!</p>
             </div>
-            <div class="">
+            <div class="mt-6 lg:mt-0">
                 <h3 class="text-lg font-semibold">Company Info</h3>
                 <ul class="mt-2 flex flex-col gap-2">
                     <li><a href="">Home</a></li>
@@ -15,7 +15,7 @@
                     <li><a href="">Membership</a></li>
                 </ul>
             </div>
-            <div class="">
+            <div class="mt-6 lg:mt-0">
                 <h3 class="text-lg font-semibold">Kontak Kami</h3>
                 <ul class="mt-2 flex flex-col gap-2">
                     <li>+626564465455</li>
@@ -24,6 +24,6 @@
                 </ul>
             </div>
         </div>
-        <p class="font-medium text-center">Copyright &copy; 2024</p>
+        <p class="font-medium mt-6 lg:mt-0 lg:text-center">Copyright &copy; 2024</p>
     </div>
 </footer>
