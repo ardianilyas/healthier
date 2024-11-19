ac79b1b3da4589387ea1c0e2d484490b1b4e887a
ardianilyas <ardianilyas@gmail.com>
2024-11-19 18:28:49 +0700

    add: image placeholder at pesan obat menu

--- resources/views/pesan/index.blade.php
+++ resources/views/pesan/index.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
     <h1 class="text-2xl font-bold">Daftar Obat</h1>
 
-    <div class="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-6">
+    <div class="my-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
         @foreach ($listObat as $obat)
             <x-card>
-                @if ($obat->image)
-                <img src="{{ asset('/storage/obat/'.$obat->image) }}" alt="{{ $obat->nama }}" width="100">
-                @endif
-                <p class="font-medium capitalize">{{ $obat->nama }}</p>  
-                <p class="text-sm font-light line-clamp-2">{{ $obat->keterangan }}</p>
+                <a href="" class="hover:bg-gray-50">
+                    <div class="flex justify-center">
+                        @if ($obat->image)
+                        <img src="{{ asset('/storage/obat/'.$obat->image) }}" alt="{{ $obat->nama }}" width="120">
+                        @else
+                        <img src="{{ asset('image-placeholder.png') }}" width="120" alt="">
+                        @endif
+                    </div>
+                    <p class="mt-3 text-lg text-neutral-800 font-medium capitalize">{{ $obat->nama }}</p>  
+                    <p class="text-sm text-neutral-600 font-light line-clamp-2">{{ $obat->keterangan }}</p>
+                </a>
             </x-card>            
         @endforeach
     </div>
