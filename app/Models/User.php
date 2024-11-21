468beb3262208813f86fee54f6fcc5c1c81b496e
ardianilyas <ardianilyas@gmail.com>
2024-11-21 20:40:39 +0700

    update: membership and memberships relations on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -45,5 +47,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function memberships() {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function membership(): HasOne {
+        return $this->hasOne(Membership::class)->latestOfMany();
     }
 }
