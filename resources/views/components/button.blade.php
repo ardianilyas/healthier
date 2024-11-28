1e76d0e0bf8931863f47527123f0ad1644a105f4
ardianilyas <ardianilyas@gmail.com>
2024-11-28 22:39:22 +0700

    update button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:bg-green-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-blue-800', 'type' => 'submit']) }}>
+<button {{ $attributes->merge(['class' => 'py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg bg-green-500 text-white hover:bg-green-600 focus:outline-none disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-blue-800', 'type' => 'submit']) }}>
     {{ $slot }}
 </button>
