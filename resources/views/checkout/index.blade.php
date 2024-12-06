35ee17bc09051e6387e340e2b49d0014cb4c28a9
ardianilyas <ardianilyas@gmail.com>
2024-12-07 00:42:31 +0700

    update: checkout view

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -6,15 +6,19 @@
 
     <x-card class="my-8">
         @if ($order->count() > 0)
-            <div class="mt-3">
+            <div class="">
+                <h2 class="text-xl font-semibold">List obat :</h2>
                 @foreach ($order->cart->items as $item)
-                    <div class="my-3">
-                        <p>Nama : {{ $item->obat->nama }}</p>
+                    <div class="my-3 bg-green-100/60 max-w-md p-4 rounded-md shadow-sm">
+                        <p>Nama obat : {{ $item->obat->nama }}</p>
                         <p>Qty : {{ $item->quantity }}</p>
                         <p>Harga satuan : Rp. {{ number_format($item->obat->harga, 2) }}</p>
                     </div>
                 @endforeach
                 <b>Total Harga : Rp. {{ number_format($order->amount, 2) }}</b>
+            </div>
+            <div class="mt-6">
+                <x-secondary-button>Bayar sekarang</x-secondary-button>
             </div>
         @else
             <p>You don't have latest checkout order for now</p>
