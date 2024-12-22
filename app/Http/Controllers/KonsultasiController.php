f485eb35a2f495b4e245eff5de740ff0139ab684
ardianilyas <ardianilyas@gmail.com>
2024-12-22 20:41:42 +0700

    update: members konsultasi limit

--- app/Http/Controllers/KonsultasiController.php
+++ app/Http/Controllers/KonsultasiController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\KonsultasiRequest;
 use App\Models\Konsultasi;
+use App\Models\Membership;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\KonsultasiRequest;
+use App\Models\MembershipLimit;
 
 class KonsultasiController extends Controller
 {
@@ -15,11 +17,20 @@ class KonsultasiController extends Controller
 
     public function konsultasi(KonsultasiRequest $request) {
         $userId = Auth::id();
+        $membership = Membership::where('user_id', $userId)->where('status', 'active')->first();
+        $membershipLimit = MembershipLimit::where('membership_id', $membership->id)->first();
+        
+        if($membershipLimit->used >= $membershipLimit->limit) {
+            return redirect()->back()->withErrors(['konsultasi' => 'Limit konsultasi sudah melebihi batas untuk periode membership saat ini']);
+        } 
 
-        $konsultasi = Konsultasi::create([
+        Konsultasi::create([
             'user_id' => $userId,
             'konsultasi' => $request->konsultasi,
         ]);
+
+        $membershipLimit->used += 1;
+        $membershipLimit->save();
 
         notify()->success('Konsultasi berhasil dikirim');
         return redirect()->back();
