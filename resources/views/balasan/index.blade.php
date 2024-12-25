0f786c076992b2851ebab9d7bec4cbd1372f0c6c
ardianilyas <ardianilyas@gmail.com>
2024-12-25 15:41:52 +0700

    update: balasan view

--- resources/views/balasan/index.blade.php
+++ resources/views/balasan/index.blade.php
@@ -7,10 +7,17 @@
         @forelse ($consultation as $konsultasi)
             <x-card>
                 <a href="{{ route('konsultasi.detail', $konsultasi->id) }}">
+                    <span class="mb-2 inline-flex">
+                        @if ($konsultasi->status === 'belum dibalas')
+                            <p class="inline-flex text-xs px-3 py-1 rounded-full bg-red-200 text-red-800">{{ $konsultasi->status }}</p>
+                        @else
+                            <p class="inline-flex text-xs px-3 py-1 rounded-full bg-green-200 text-green-800">{{ $konsultasi->status }}</p>
+                        @endif
+                    </span>
                     <p class="text-sm text-neutral-600">{{ $konsultasi->created_at->diffForHumans() }}</p>
                     <p><b>Nama pasien :</b> {{ $konsultasi->user->name }}</p>
-                    <p href="" class="line-clamp-3"><b>Keluhan : </b>{{ $konsultasi->konsultasi }}</p>
-                    <p><b>Status : </b>{{ $konsultasi->status }}</p>
+                    <p class="line-clamp-3"><b>Keluhan : </b>{{ $konsultasi->konsultasi }}</p>
+                    {{-- <p><b>Status : </b>{{ $konsultasi->status }}</p> --}}
                 </a>
             </x-card>
         @empty
